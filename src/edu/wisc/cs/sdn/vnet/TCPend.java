b7e1d32121482b72a834463998d24f1c73b080ee
LocalBrian <150453691+LocalBrian@users.noreply.github.com>
2025-04-09 01:45:17 +0000

    Minor changes to update to assignment 4.

---
+++ src/edu/wisc/cs/sdn/vnet/TCPend.java
@@ -0,0 +1,8 @@
+package edu.wisc.cs.sdn.vnet;
+
+public class TCPend {
+    public static void main(String[] args) {
+        System.out.println("Hello from TCPend!");
+        // your code here
+    }
+}
